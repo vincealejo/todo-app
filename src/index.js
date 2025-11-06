43023f2a8712caf6214ea9bc493d16c789ec93b1
vince <219931481+vincealejo@users.noreply.github.com>
2025-11-06 11:31:00 +0800

    Add: (UI) write task ui initial design idea

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
+import "./styles.css";
 import TODO_APP from "./modules/todo-app.js";
 
 // this needs to be called first
 TODO_APP.init();
-
-// TODO_APP.createProject("project two");
-// TODO_APP.createTask({
-//     title: "task one",
-//     description: "desc one",
-//     due: "due one",
-//     priority: "prio one",
-// });
-// TODO_APP.deleteProject("672d5199-4ee4-4bba-8555-f87801fd91d7");
---
+++ src/modules/ui.js
@@ -0,0 +1,17 @@
+export default class UI {
+    static app = document.querySelector('#app');
+    static taskContainer = document.querySelector("#task-container");
+
+    static displayTasks(tasks) {
+        tasks
+    }
+
+    static #createTask(task) {
+        const container = document.createElement("arcticle");
+        container.className = "task";
+
+        const title = document.createElement("p");
+        title.className = "task-title";
+
+    }
+}
